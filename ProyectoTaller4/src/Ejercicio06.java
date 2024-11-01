72f9170e7ceaa89bfc609cb99a4ba0992c545ed4
DalejaBri <dalejabriceno@gmail.com>
2024-11-01 13:57:21 -0500

    actualizacion

--- ProyectoTaller4/src/Ejercicio06.java
+++ ProyectoTaller4/src/Ejercicio06.java
@@ -9,10 +9,10 @@ public class Ejercicio06 {
         Scanner cobro = new Scanner(System.in);
         System.out.print("Ingrese la cantidad del prestamo: ");
         prestamo = cobro.nextDouble();
-        System.out.print("Ingrese el interes mensual: ");
+        System.out.print("Ingrese el interes mensual (en decimales): ");
         interesMensual = cobro.nextDouble();
         interesMensual = interesMensual / 100;
-        pagoMensual = (prestamo * interesMensual) / (1 - Math.pow(1 + interesMensual, -numeroPagos  ));
+        pagoMensual = (prestamo * interesMensual) / (1 - Math.pow(1 + interesMensual, -numeroPagos));
         System.out.println("El pago mensual del prestamo es: " + pagoMensual);
     }
 }
--- ProyectoTaller4/src/Ejercicio08.java
+++ ProyectoTaller4/src/Ejercicio08.java
@@ -1,5 +1,24 @@
-
+import java.util.Scanner;
 public class Ejercicio08 {
-    
+    public static void main(String[] args) {
+        double netflix, youtubePremium, dropbox, spotify, pagoTotal;
+        int edad;
+        Scanner servicios = new Scanner(System.in);
+        System.out.print("Ingrese su edad: ");
+        edad = servicios.nextInt();
+        System.out.print("Ingrese el pago mensual de Netflix: ");
+        netflix = servicios.nextDouble();
+        System.out.print("Ingrese el pago mensual de Youtube Premium: ");
+        youtubePremium = servicios.nextDouble();
+        System.out.print("Ingrese el pago mensual de Dropbox: ");
+        dropbox = servicios.nextDouble();
+        System.out.print("Ingrese el pago mensual de Spotify: ");
+        spotify = servicios.nextDouble();
+        pagoTotal = netflix + youtubePremium + dropbox + spotify;
+        if (edad < 30) { 
+            pagoTotal *= 0.8;
+        }
+        System.out.println("El total a pagar mensualmente es: " + pagoTotal);
     }
+}
 
